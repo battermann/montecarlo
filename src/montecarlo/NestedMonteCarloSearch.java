83889a8de644bcc52b95b442dd3809ff4142130a
Leif Battermann <leif.battermann@arcor.de>
2016-06-07 10:03:25 +0200

    mistake corrected

--- src/montecarlo/NestedMonteCarloSearch.java
+++ src/montecarlo/NestedMonteCarloSearch.java
@@ -10,11 +10,11 @@ public class NestedMonteCarloSearch {
 			final int level, final Supplier<Boolean> isCanceled) {
 
 		// terminating case
-		if(level <= 0) 
+		if (level <= 0)
 			return state.simulation();
-		
+
 		Pair<Double, List<TAction>> globalBestResult = Pair.of(state.getScore(), new ArrayList<TAction>());
-		final List<TAction> previousAppliedActions = new ArrayList<TAction>();
+		final List<TAction> visitedNodes = new ArrayList<TAction>();
 
 		while (!state.isTerminalPosition() && !isCanceled.get()) {
 
@@ -23,8 +23,8 @@ public class NestedMonteCarloSearch {
 
 			for (TAction action : state.findAllLegalActions()) {
 				final INmcsState<TState, TAction> currentState = state.takeAction(action);
-				final Pair<Double, List<TAction>> simulationResult = executeSearch(currentState, level - 1,
-						isCanceled);
+				// recursion
+				final Pair<Double, List<TAction>> simulationResult = executeSearch(currentState, level - 1, isCanceled);
 
 				if (simulationResult.item1 >= currentBestResult.item1) {
 					currentBestAction = action;
@@ -32,11 +32,13 @@ public class NestedMonteCarloSearch {
 				}
 			}
 
-			previousAppliedActions.add(currentBestAction);
-
-			if (currentBestResult.item1 > globalBestResult.item1) {
+			if (currentBestResult.item1 >= globalBestResult.item1) {
+				visitedNodes.add(currentBestAction);
 				globalBestResult = currentBestResult;
-				globalBestResult.item2.addAll(0, previousAppliedActions);
+				globalBestResult.item2.addAll(0, visitedNodes);
+			} else {
+				currentBestAction = globalBestResult.item2.get(visitedNodes.size());
+				visitedNodes.add(currentBestAction);
 			}
 
 			state = state.takeAction(currentBestAction);
